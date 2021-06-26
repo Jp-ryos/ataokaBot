7f169e905f38091b0503cc7747fd1d1b8f0c3a1d
Ryo <40242414+Jp-ryos@users.noreply.github.com>
2021-06-26 09:01:40 +0900

    feat: display  nowTime

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import asyncio
 import os
 import traceback
+import datetime
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -28,10 +29,10 @@ async def command(ctx):
     await ctx.send('> -----コマンド一覧-----\n- /test_ping 疎通確認を行うコマンドです ping-pong!!!。 \n- /rect 募集を行うコマンドです。\n サーバと同期できなくなるので何度もボタンを連打するのはお控えください。\n\t 例） /rect Apex 4 500 \n\t 　　 /rect [arg0: 募集要項] [arg1: 人数] [arg2: 募集する時間（秒）]')
 
 @bot.command()
-async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
+async def rect(ctx, about = "募集\n" + "(募集開始：" + datetime.datetime.now().strftime('%Y/%m/%d %H:%M') + ")", cnt = 4, settime = 10.0):
     cnt, settime = int(cnt), float(settime)
     reaction_member = [">>>"]
-    test = discord.Embed(title=about,colour=0x1e90ff)
+    test = discord.Embed(title=about + "\n(募集開始：" + datetime.datetime.now().strftime('%Y/%m/%d %H:%M') + ")\n",colour=0x1e90ff)
     test.add_field(name=f"あと{cnt}人 募集中\n", value=None, inline=True)
     msg = await ctx.send(embed=test)
     #投票の欄
