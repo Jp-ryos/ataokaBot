d2f803f9ef208760395106c3a74a30e43406c767
Ryo <40242414+Jp-ryos@users.noreply.github.com>
2021-06-26 09:17:57 +0900

    fix: fixed bug when datetime display

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,7 @@ import asyncio
 import os
 import traceback
 import datetime
+from datetime import timedelta
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -29,13 +30,14 @@ async def command(ctx):
     await ctx.send('> -----コマンド一覧-----\n- /test_ping 疎通確認を行うコマンドです ping-pong!!!。 \n- /rect 募集を行うコマンドです。\n サーバと同期できなくなるので何度もボタンを連打するのはお控えください。\n\t 例） /rect Apex 4 500 \n\t 　　 /rect [arg0: 募集要項] [arg1: 人数] [arg2: 募集する時間（秒）]')
 
 @bot.command()
-async def rect(ctx, about = "募集\n", cnt = 4, settime = 10.0 * 60):
-    cnt, settime, now, end = int(cnt), float(settime * 60), datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), datetime.datetime.now()+settime
+async def rect(ctx, about = "募集\n", cnt = 4, settime = 10.0):
+    cnt, settime, now = int(cnt), float(settime * 60), datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    end = now + timedelta(0, settime, 0)
     reaction_member = [">>>"]
     test = discord.Embed(title=about,colour=0x1e90ff)
     test.add_field(name=f"あと{cnt}人 募集中\n", value=None, inline=True)
     test.add_field(name=f"(募集開始：{now})\n", value=None, inline=True)
-    test.add_field(name=f"(募集終了：{end})\n", value=None, inline=True)
+    test.add_field(name=f"(募集終了予定：{end.strftime('%Y/%m/%d %H:%M:%S')})\n", value=None, inline=True)
     msg = await ctx.send(embed=test)
     #投票の欄
     await msg.add_reaction('⏫')
